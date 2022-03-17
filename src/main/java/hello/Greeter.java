0d8c80688e2eb27926319c7393a0ac05341713c7
Stanisław Serwin <stanserw@wp.pl>
2022-03-17 20:26:22 +0100

    zmiana package dla klas

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -1,4 +1,4 @@
-package main.java.hello;
+package hello;
 
 public class Greeter {
     public String sayHello() {
--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -2,7 +2,7 @@ package hello;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        main.java.hello.Greeter greeter = new main.java.hello.Greeter();
+        Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
     }
 }
