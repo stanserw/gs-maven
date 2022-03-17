bf8b5ce91e068b93210131beae7e4f7b9a6cdb23
Stanisław Serwin <stanserw@wp.pl>
2022-03-17 20:22:11 +0100

    Initial commit

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,8 @@
+package hello;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        main.java.hello.Greeter greeter = new main.java.hello.Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
